93d32c55079eb21a0ed509bd06f67254b91ed557
ddr533 <ddr533@yandex.ru>
2022-10-31 17:37:17 +0700

    Create class AddPostForm(forms.ModelForm) for addpage.html

--- startsite/women/forms.py
+++ startsite/women/forms.py
@@ -1,9 +1,31 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import *
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, label="URL")
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), required=False, label="Контент")
-    is_published = forms.BooleanField(label="Публикация", initial=True)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
+# class AddPostForm(forms.Form):
+#     title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
+#     slug = forms.SlugField(max_length=255, label="URL")
+#     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), required=False, label="Контент")
+#     is_published = forms.BooleanField(label="Публикация", initial=True)
+#     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
+
+class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = "Категория не выбрана"
+
+    class Meta:
+        model = Women
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) < 200:
+            raise ValidationError('Длина превышает 200 символов')
+
+        return title
--- startsite/women/views.py
+++ startsite/women/views.py
@@ -6,7 +6,7 @@ from women.models import *
 
 
 def index(request):
-    posts = Women.objects.all()
+    posts = Women.objects.all().order_by('-time_create')
     context = {
         'posts': posts,
         'title': 'Главная страница',
@@ -16,7 +16,7 @@ def index(request):
 
 def show_category(request, cat_slug):
     cat_id = Category.objects.filter(slug=cat_slug)[0].pk
-    posts = Women.objects.filter(cat_id=cat_id)
+    posts = Women.objects.filter(cat_id=cat_id).order_by('-time_create')
     if len(posts) == 0:
         raise Http404()
     context = {
@@ -36,19 +36,27 @@ def show_post(request, post_slug):
     }
     return render(request, 'women/post.html', context=context)
 
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             try:
+#                 Women.objects.create(**form.cleaned_data)
+#                 return redirect('home')
+#             except:
+#                 form.add_error(None, 'Ошибка добавления поста')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'women/addpage.html', {'form': form, 'title': 'Добавление статьи'})
+
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
-            #print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
-
     return render(request, 'women/addpage.html', {'form': form, 'title': 'Добавление статьи'})
 
 def about(request):
