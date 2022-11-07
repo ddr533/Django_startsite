0acaedcee868b0696d7d3f95fc3ac82ded66e905
ddr533 <ddr533@yandex.ru>
2022-11-07 22:37:45 +0700

    Modify admin page

--- startsite/women/admin.py
+++ startsite/women/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from women.models import *
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published', 'slug', 'cat')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published', 'slug', 'cat')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
+    admin.site.site_title = 'Админ-панель сайта о женщинах'
+    admin.site.site_header = 'Админ-панель сайта о женщинах'
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = "Миниатюра"
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug')
