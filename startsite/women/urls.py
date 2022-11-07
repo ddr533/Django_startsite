6aeacc4c9554eb770a0527ab24ec3b8d3d2fe7fd
ddr533 <ddr533@yandex.ru>
2022-11-07 19:42:07 +0700

    Add contact page

--- startsite/women/urls.py
+++ startsite/women/urls.py
@@ -9,12 +9,13 @@ urlpatterns = [
     path('', WomenHome.as_view(), name='home'),
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
-    path('contact/', contact, name='contact'),
+    #path('contact/', contact, name='contact'),
     path('login/', LoginUser.as_view(), name='login'),
     path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('logout/', logout_user, name='logout'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
 ]
 
 """Function View"""
